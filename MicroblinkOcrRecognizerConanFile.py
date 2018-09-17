23c0c924c98d9827ff7048457a3f684cef7f2ecb
Nenad Mikša <nenad.miksa@microblink.com>
2018-09-17 10:43:43 +0200

    add enable_testing option to base recognizer conanfile

--- MicroblinkOcrRecognizerConanFile.py
+++ MicroblinkOcrRecognizerConanFile.py
@@ -7,5 +7,7 @@ class MicroblinkRecognizerConanFile(base.MicroblinkRecognizerConanFile):
     def configure(self):
         self.options['Recognizer'].result_jsonization = self.options.result_jsonization
         self.options['Recognizer'].binary_serialization = self.options.binary_serialization
+        self.options['Recognizer'].enable_testing = self.options.enable_testing
         self.options['BlinkInputRecognizer'].result_jsonization = self.options.result_jsonization
         self.options['BlinkInputRecognizer'].binary_serialization = self.options.binary_serialization
+        self.options['BlinkInputRecognizer'].enable_testing = self.options.enable_testing
--- MicroblinkRecognizerConanFile.py
+++ MicroblinkRecognizerConanFile.py
@@ -6,8 +6,9 @@ class MicroblinkRecognizerConanFile(base.MicroblinkConanFile):
     options = dict(base.MicroblinkConanFile.options, **{
         'result_jsonization': ['Off', 'Serialization', 'SerializationAndTesting'],
         'binary_serialization': [True, False],
+        'enable_testing': [True, False]
     })
-    default_options = ('result_jsonization=Off',) + base.MicroblinkConanFile.default_options
+    default_options = ('result_jsonization=Off', 'enable_testing=False') + base.MicroblinkConanFile.default_options
 
 
     def config_options(self):
@@ -21,12 +22,14 @@ class MicroblinkRecognizerConanFile(base.MicroblinkConanFile):
     def configure(self):
         self.options['Recognizer'].result_jsonization = self.options.result_jsonization
         self.options['Recognizer'].binary_serialization = self.options.binary_serialization
+        self.options['Recognizer'].enable_testing = self.options.enable_testing
 
 
     def common_recognizer_build_args(self):
         cmake_args = [
             f'-DRecognizer_RESULT_JSONIZATION={self.options.result_jsonization}',
-            f'-DRecognizer_BINARY_SERIALIZATION={self.options.binary_serialization}'
+            f'-DRecognizer_BINARY_SERIALIZATION={self.options.binary_serialization}',
+            f'-DRecognizer_ENABLE_TESTING={self.options.enable_testing}'
         ]
         return cmake_args
 
