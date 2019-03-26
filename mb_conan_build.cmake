12e3ad420b18adfbbbab46123f13cbc657167bd5
Nenad Mikša <nenad.miksa@microblink.com>
2019-03-26 18:56:53 +0100

    ensure HAVE_PROFILE is set to ON when using macos

--- mb_conan_build.cmake
+++ mb_conan_build.cmake
@@ -90,6 +90,7 @@ else() # in user space and user has not performed conan install command
         set( HAVE_PROFILE ON )
     elseif( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
         list( APPEND conan_cmake_run_params PROFILE macos )
+        set( HAVE_PROFILE ON )
     endif()
 
     if( MB_CONAN_SETUP_PARAMS )
